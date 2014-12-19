109da935288f1fd174a7ce388b9170f1926fae47
Abhilasha Gupta <agupta@godaddy.com>
2014-12-18 19:28:18 -0800

    Tried to load the test to break it. It still works under the load

--- src/test/java/com/godaddy/domains/integration/java/AsyncHandlerEventBusTest.java
+++ src/test/java/com/godaddy/domains/integration/java/AsyncHandlerEventBusTest.java
@@ -22,13 +22,45 @@ import static org.vertx.testtools.VertxAssert.testComplete;
  * Purpose :
  */
 public class AsyncHandlerEventBusTest extends TestVerticle {
-    @Override
-    public void start() {
-        // Make sure we call initialize() - this sets up the assert stuff so assert functionality works correctly
-        initialize();
 
-        // Deploy the module - the System property `vertx.modulename` will contain the name of the module so you
-        // don't have to hardecode it in your tests
+
+    @Test
+      public void testAsyncMessageCallSucess()
+    {
+        container.deployVerticle(AsyncHandlerEventBusWithSuccess.class.getName(), new AsyncResultHandler<String>() {
+            @Override
+            public void handle(AsyncResult<String> asyncResult) {
+                // Deployment is asynchronous and this this handler will be called when it's complete (or failed)
+                assertTrue(asyncResult.succeeded());
+                assertNotNull("deploymentID should not be null", asyncResult.result());
+
+            }
+        });
+        for (int i = 0; i < 500 ; i++) {
+
+            vertx.eventBus().sendWithTimeout("test.success", "This is a message", 1000, new Handler<AsyncResult<Message<String>>>() {
+                public void handle(AsyncResult<Message<String>> result) {
+                    if (result.succeeded()) {
+                        System.out.println("I received a reply " + result.result().body());
+                        assertEquals("WOOT WOOT WOOT INDEED", result.result().body());
+
+                    } else {
+                        ReplyException ex = (ReplyException) result.cause();
+                        System.err.println("Failure type: " + ex.failureType());
+                        System.err.println("Failure code: " + ex.failureCode());
+                        System.err.println("Failure message: " + ex.getMessage());
+                        fail();
+
+                    }
+                }
+            });
+            testComplete();
+        }
+    }
+
+    @Test
+    public void testAsyncMessageCall()
+    {
 
         container.deployVerticle(AsyncHandlerEventBusWithFailure.class.getName(), new AsyncResultHandler<String>() {
             @Override
@@ -39,59 +71,27 @@ public class AsyncHandlerEventBusTest extends TestVerticle {
 
             }
         });
+        for (int i = 0; i < 500 ; i++) {
 
-        container.deployVerticle(AsyncHandlerEventBusWithSuccess.class.getName(), new AsyncResultHandler<String>() {
-            @Override
-            public void handle(AsyncResult<String> asyncResult) {
-                // Deployment is asynchronous and this this handler will be called when it's complete (or failed)
-                assertTrue(asyncResult.succeeded());
-                assertNotNull("deploymentID should not be null", asyncResult.result());
-                // If deployed correctly then start the tests!
-                startTests();
-            }
-        });
-    }
-
-    @Test
-      public void testAsyncMessageCallSucess()
-    {
-        vertx.eventBus().sendWithTimeout("test.success", "This is a message", 1000, new Handler<AsyncResult<Message<String>>>() {
-            public void handle(AsyncResult<Message<String>> result) {
-                if (result.succeeded()) {
-                    System.out.println("I received a reply " + result.result().body());
-                    assertEquals("WOOT WOOT WOOT INDEED", result.result().body());
-                    testComplete();
-                } else {
-                    ReplyException ex = (ReplyException) result.cause();
-                    System.err.println("Failure type: " + ex.failureType());
-                    System.err.println("Failure code: " + ex.failureCode());
-                    System.err.println("Failure message: " + ex.getMessage());
-                    fail();
-                    testComplete();
-                }
-            }
-        });
-    }
-
-    @Test
-    public void testAsyncMessageCall()
-    {
         vertx.eventBus().sendWithTimeout("test.failure", "This is a message", 1000, new Handler<AsyncResult<Message<String>>>() {
             public void handle(AsyncResult<Message<String>> result) {
                 if (result.succeeded()) {
 
                     System.out.println("I received a reply " + result.result().body());
                     fail();
-                    testComplete();
+
                 } else {
                     ReplyException ex = (ReplyException) result.cause();
                     System.err.println("Failure type: " + ex.failureType());
                     System.err.println("Failure code: " + ex.failureCode());
                     System.err.println("Failure message: " + ex.getMessage());
                     assertEquals(123, ex.failureCode());
-                    testComplete();
+
                 }
             }
+
         });
+            testComplete();
+        }
     }
 }
